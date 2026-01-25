25a6202e2b039f873a21590173bfe37ed348bd13
Ahmet Çetinkaya <ahmetcetinkaya@tutamail.com>
2026-01-25 21:31:17 +0300

    fix(color): always return 6 digits in toHexString

--- lib/extensions/color_extensions.dart
+++ lib/extensions/color_extensions.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 extension ColorExtensions on Color {
   /// Converts a Color to a hex string without the alpha channel.
   String toHexString() {
-    final hexValue = toARGB32().toRadixString(16).toUpperCase();
-    return hexValue.length == 8 ? hexValue.substring(2) : hexValue;
+    return toARGB32().toRadixString(16).padLeft(8, '0').substring(2).toUpperCase();
   }
 }
